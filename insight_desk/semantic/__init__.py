336e93114f701c80fad123e7d8a8f3fd143a7297
kjkwon981229-prog <kjkwon981229@gmail.com>
2026-08-23 04:23:42 +0900

    Restore semantic exports after rejected canary

--- insight_desk/semantic/__init__.py
+++ insight_desk/semantic/__init__.py
@@ -2,16 +2,13 @@
 
 from .evidence import EvidenceSegmenter
 from .facts import FactDraft, FactExtractionRequest, FactExtractorPort
-from .groq_extractor import FACT_EXTRACTION_SCHEMA, Groq20BFactExtractor
 from .pipeline import SemanticArticleResult, SemanticPipeline
 
 __all__ = [
     "EvidenceSegmenter",
-    "FACT_EXTRACTION_SCHEMA",
     "FactDraft",
     "FactExtractionRequest",
     "FactExtractorPort",
-    "Groq20BFactExtractor",
     "SemanticArticleResult",
     "SemanticPipeline",
 ]
